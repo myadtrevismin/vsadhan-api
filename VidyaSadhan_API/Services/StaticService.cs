426be26f406b94f1e88ba737af59090c2d555f53
hemanth43 <hemanth43@gmail.com>
2020-09-07 20:54:12 -0400

    Created a get method for static subjects

--- VidyaSadhan_API/Services/StaticService.cs
+++ VidyaSadhan_API/Services/StaticService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using VidyaSadhan_API.Entities;
 using VidyaSadhan_API.Extensions;
+using VidyaSadhan_API.Models;
 
 namespace VidyaSadhan_API.Services
 {
@@ -26,6 +27,16 @@ namespace VidyaSadhan_API.Services
         public IEnumerable<State> GetStates()
         {
             return _dbContext.States.ToList();
+        }
+
+        public StaticDataForSubjectsViewModel GetStaticDataForSubjects()
+        {
+            var result = new StaticDataForSubjectsViewModel();
+            result.Groups = _dbContext.Groups.ToList();
+            result.Mediums = _dbContext.Mediums.ToList();
+            result.States = _dbContext.States.ToList();
+            result.Subjects = _dbContext.Subjects.ToList();
+            return result;
         }
     }
 }
